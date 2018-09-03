3abe9147dbd1e39c79a2fadb54311d93c8de17aa
Ishav Punia <ishav.punia@gmail.com>
2018-09-04 00:33:19 +0530

    Try this simple app client server app out

---
+++ flasktest.py
@@ -0,0 +1,13 @@
+import flask
+import pyautogui
+import ctypes
+import time
+app=flask.Flask(__name__)
+
+@app.route("/run/<number>",methods=["GET"])
+def run_command(number):
+	
+	num = float(number)		#number is grabbed from the resource of app.route but it is a string so convert to a float
+	square=num*num
+	response=flask.Response(str(square),headers={"Access-Control-Allow-Origin":"*"})		#The code won't work without this(related to cross origin resource sharing)
+	return response
